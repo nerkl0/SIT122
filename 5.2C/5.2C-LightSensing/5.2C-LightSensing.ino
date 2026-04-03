ccc390cbe32f37c1c396b68d8b62d9e03de2336c
Nicole <nicolebirchh@gmail.com>
2026-04-03 13:06:00 +1100

    Removed unnecessary code. Adding readSensors() and global sensor variables to make it easier to update in different states

--- 5.2C/5.2C-LightSensing/5.2C-LightSensing.ino
+++ 5.2C/5.2C-LightSensing/5.2C-LightSensing.ino
@@ -5,12 +5,13 @@ MeLightSensor lightSensor1(11); // left
 MeLightSensor lightSensor2(12); // right
 
 const int SPEED = 120;
+const int TURN_SPEED = 200;
 const int LIGHT_TOLERANCE = 30;
 
 enum State { TURN, DRIVE, STOP };
 State state = STOP;
-int turnDirection = 0;
 int previousLight = -1; // variable to calculate ranger to stop once at light source
+int sensor1, sensor2, lightImbalance;
 
 // non blocking delay for motors to continue running via _loop()
 void _delay(float seconds) {
@@ -19,6 +20,13 @@ void _delay(float seconds) {
   }
   long endTime = millis() + seconds * 1000;
   while(millis() < endTime) _loop();
+}
+
+// Handles reading and updating global sensor/lightImbalance variables
+void readSensors() {
+  sensor1 = lightSensor1.read();
+  sensor2 = lightSensor2.read();
+  lightImbalance = sensor1 - sensor2;
 }
 
 void setup() {
@@ -34,45 +42,48 @@ void setup() {
 
 void loop() {
   _loop();
+  readSensors();
 
-  int sensor1 = lightSensor1.read();
-  int sensor2 = lightSensor2.read();
-  int lightImbalance = sensor1 - sensor2;
-
-  // check light imbalance & drive towards souce of light. 
-  // Triggers DRIVE if lightImbalance is within the light_tolerance range
-  // DRIVE will then check whether the light imbalance has changed triggering TURN
-  if (abs(lightImbalance) <= LIGHT_TOLERANCE) {
-    previousLight = sensor1 + sensor2;
-    state = DRIVE;
-  }
+  // Begins in a STOP state. Will only start moving if it detects a change in light
+  if (abs(lightImbalance) > LIGHT_TOLERANCE && state != STOP)
+    state = TURN; 
 
   switch (state) {
+    // Decides which direction to turn by evaluating whether the imbalance is coming from
+    // the left or right sensor. Continues turning until it's detected the source of the light
     case TURN:
-      turnDirection = lightImbalance > 0 ? 4 : 3; // sets left / right turn
-      move(turnDirection, SPEED*1.75);
+      int turnDirection = lightImbalance > 0 ? 4 : 3;
+      while (abs(lightImbalance) > LIGHT_TOLERANCE) {
+        move(turnDirection, TURN_SPEED);
+        _loop();
+        readSensors();
+      }
       break;
 
+    // Handles moving towards the light. Current and previous light detect whether the light source has been passed 
+    // If current light is less than the previous, it means the light source has been found and the robot stops moving
+    // If an imbalance is detected, indicates the light source has moved, state set to STOP
+    // Otherwise will continue to update previousLight while it's greater than current
     case DRIVE:{
-      if (abs(lightImbalance) > LIGHT_TOLERANCE) {
-        state = TURN;
-      } else {
-        move(1, SPEED);
-        _delay(0.4);
-        
-        // rechecks sensors and compares against the previous light level
-        // if detected to be moving away from the light source will stop
-        int current = lightSensor1.read() + lightSensor2.read();
-        if (current < previousLight)
-          state = STOP;
-        else
-          previousLight = current;
-        }
+      move(1, SPEED);
+      _delay(0.5);
+      readSensors();
+
+      int current = sensor1 + sensor2;
+      if (abs(lightImbalance) > LIGHT_TOLERANCE) state = TURN;
+      else if (current < previousLight) state = STOP;
+      else previousLight = current;
+
       break;
     }
+
     case STOP:
       stopMotors();
       _delay(0.5);
+      
+      readSensors();
+      if (abs(lightImbalance) > LIGHT_TOLERANCE)
+        state = TURN;
       break;
   }
 }
